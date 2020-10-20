af40fd14f4eb07001ef421e1809af77370c8fe87
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-20 11:37:08 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan15-Logika/src/if1/pkg10119004/latihan15/logika/IF110119004Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan15.logika;
+
+/**
+ *
+ * @author ENDOG
+ * NAMA     : Muhamad Ramadan
+ * KELAS    : IF 1
+ * NIM      : 10119004
+ * Deskripsi Program : Program ini berisi untuk menampilkan
+ * value ke layar
+ */
+public class IF110119004Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println(" a && b = " + (a && b));
+        System.out.println(" a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
